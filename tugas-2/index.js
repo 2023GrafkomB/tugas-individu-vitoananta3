2108f5c9b4e7bfff8eff27c041c2080f4ec71bb3
Vito Febrian Ananta <vitoananta3@gmail.com>
2023-09-23 13:35:10 +0700

    Add more comments on index.js

--- tugas-2/index.js
+++ tugas-2/index.js
@@ -1,17 +1,16 @@
+// Function to initialize WebGL and set up the scene
 var init = function () {
+    // Get a reference to the canvas element and create a WebGL rendering context
     var canvas = document.getElementById('canvas');
     var gl = canvas.getContext('webgl');
 
-    // gl.clearColor(0.75, 0.85, 0.8, 1.0);
-    // gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    // Enable depth testing and backface culling for better rendering
     gl.enable(gl.DEPTH_TEST);
     gl.enable(gl.CULL_FACE);
     gl.frontFace(gl.CCW);
     gl.cullFace(gl.BACK);
 
-    //
-    // Create shaders
-    // 
+    // Load and compile vertex and fragment shaders
     var vertexShaderText = document.getElementById('vertex-shader').textContent;
     var fragmentShaderText = document.getElementById('fragment-shader').textContent;
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
@@ -23,40 +22,39 @@ var init = function () {
     gl.compileShader(vertexShader);
     gl.compileShader(fragmentShader);
 
+    // Create a shader program and link the shaders
     var program = gl.createProgram();
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragmentShader);
     gl.linkProgram(program);
     gl.validateProgram(program);
 
-    //
-    // Create buffer
-    //
-    var pyramidVertices =
-        [ //    X,      Y,      Z               U,      V
-            //  Base
-            -1.0, -1.0, -1.0, 0, 0,
-            1.0, -1.0, -1.0, 1, 0,
-            1.0, -1.0, 1.0, 1, 1,
-            -1.0, -1.0, 1.0, 0, 1,
+    // Define pyramid vertices and indices
+    var pyramidVertices = [
+        // X, Y, Z, U, V
+        // Base
+        -1.0, -1.0, -1.0, 0, 0,
+        1.0, -1.0, -1.0, 1, 0,
+        1.0, -1.0, 1.0, 1, 1,
+        -1.0, -1.0, 1.0, 0, 1,
 
-            // Front
-            0.0, 1.0, 0.0, 0.5, 0.5,
-        ];
+        // Front
+        0.0, 1.0, 0.0, 0.5, 0.5,
+    ];
 
-    var pyramidIndices =
-        [
-            // Base
-            0, 1, 2,
-            0, 2, 3,
+    var pyramidIndices = [
+        // Base
+        0, 1, 2,
+        0, 2, 3,
 
-            // Front
-            0, 4, 1,
-            1, 4, 2,
-            2, 4, 3,
-            3, 4, 0,
-        ];
+        // Front
+        0, 4, 1,
+        1, 4, 2,
+        2, 4, 3,
+        3, 4, 0,
+    ];
 
+    // Create vertex and index buffer objects
     var pyramidVertexBufferObject = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, pyramidVertexBufferObject);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(pyramidVertices), gl.STATIC_DRAW);
@@ -65,14 +63,17 @@ var init = function () {
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, pyramidIndexBufferObject);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(pyramidIndices), gl.STATIC_DRAW);
 
+    // Get attribute locations for shader program
     var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
     var texCoordAttribLocation = gl.getAttribLocation(program, 'vertTexCoord');
+
+    // Specify how to interpret vertex data
     gl.vertexAttribPointer(
         positionAttribLocation, // Attribute location
         3, // Number of elements per attribute
         gl.FLOAT, // Type of elements
         gl.FALSE,
-        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+        5 * Float32Array.BYTES_PER_ELEMENT, // Size of a single vertex
         0 // Offset from the beginning of a single vertex to this attribute
     );
     gl.vertexAttribPointer(
@@ -80,34 +81,37 @@ var init = function () {
         2, // Number of elements per attribute
         gl.FLOAT, // Type of elements
         gl.FALSE,
-        5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+        5 * Float32Array.BYTES_PER_ELEMENT, // Size of a single vertex
         3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
     );
 
+    // Enable attribute arrays
     gl.enableVertexAttribArray(positionAttribLocation);
     gl.enableVertexAttribArray(texCoordAttribLocation);
 
-    //
-    // Create texture
-    //
+    // Create a texture and configure its properties
     var pyramidTexture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, pyramidTexture);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+    // Load an image into the texture
     gl.texImage2D(
         gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, document.getElementById('texture')
-    )
+    );
     gl.bindTexture(gl.TEXTURE_2D, null);
 
-    // Tell OpenGL state machine which program should be active.
+    // Specify the shader program to use
     gl.useProgram(program);
 
+    // Get uniform locations for transformation matrices
     var matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
     var matViewUniformLocation = gl.getUniformLocation(program, 'mView');
     var matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
 
+    // Create transformation matrices
     var worldMatrix = new Float32Array(16);
     var viewMatrix = new Float32Array(16);
     var projMatrix = new Float32Array(16);
@@ -115,26 +119,21 @@ var init = function () {
     mat4.lookAt(viewMatrix, [0, 0, -8], [0, 0, 0], [0, 1, 0]);
     mat4.perspective(projMatrix, glMatrix.toRadian(45), canvas.clientWidth / canvas.clientHeight, 0.1, 1000.0);
 
+    // Set the transformation matrices as uniform values
     gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
     gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
     gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
 
+    // Define rotation matrices for animation
     var xRotationMatrix = new Float32Array(16);
     var yRotationMatrix = new Float32Array(16);
 
-    //
-    // Main render loop
-    //
+    // Initialize animation variables
     var identityMatrix = new Float32Array(16);
     mat4.identity(identityMatrix);
     var angle = 0;
-    // var animationRequestId; // Store the requestAnimationFrame ID
-
-    let animationRunning = false;
-    let animationRequestId;
-    if (animationRunning == false) {
-        document.getElementById("stopButton").disabled = true;
-    }
+    var animationRunning = false;
+    var animationRequestId;
 
     // Function to start the animation
     function startAnimation() {
@@ -151,12 +150,14 @@ var init = function () {
                 // Update the angle based on delta time
                 angle += deltaTime * Math.PI; // Adjust the speed as needed
 
+                // Rotate the pyramid
                 mat4.rotate(yRotationMatrix, identityMatrix, angle, [0, 1, 0]);
                 mat4.rotate(xRotationMatrix, identityMatrix, angle / 3, [1, 0, 0]);
                 mat4.mul(worldMatrix, yRotationMatrix, xRotationMatrix);
                 mat4.scale(worldMatrix, worldMatrix, [scaleFactor, scaleFactor, scaleFactor]);
                 gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
 
+                // Clear the canvas and draw the pyramid
                 gl.clearColor(0, 0, 0, 1);
                 gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 
@@ -176,7 +177,6 @@ var init = function () {
             var previousTime = performance.now();
             animate(previousTime);
         }
-
     }
 
     // Function to stop the animation
@@ -193,7 +193,7 @@ var init = function () {
     document.getElementById('startButton').addEventListener('click', startAnimation);
     document.getElementById('stopButton').addEventListener('click', stopAnimation);
 
-    // Add keyboard event listeners
+    // Add keyboard event listeners to start and stop animation
     document.addEventListener("keydown", function (event) {
         if (event.key === "a" || event.key === "A") {
             startAnimation();
@@ -210,28 +210,23 @@ var init = function () {
     var scaleMatrix = new Float32Array(16);
     mat4.identity(scaleMatrix);
 
-
     // Function to update the scale when the slider value changes
     scaleSlider.addEventListener('input', function () {
         scaleFactor = parseFloat(scaleSlider.value);
         scaleValue.textContent = scaleFactor.toFixed(1);
 
-        // Update the scale matrix
+        // Update the scale matrix and redraw the object
         mat4.identity(scaleMatrix);
         mat4.scale(scaleMatrix, scaleMatrix, [scaleFactor, scaleFactor, scaleFactor]);
-
-        // Redraw the object
         gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
         mat4.mul(worldMatrix, yRotationMatrix, xRotationMatrix);
-
-        // Apply both the scale and rotation transformations
         mat4.mul(worldMatrix, scaleMatrix, worldMatrix);
         gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
         gl.drawElements(gl.TRIANGLES, pyramidIndices.length, gl.UNSIGNED_SHORT, 0);
-
         updateTranslation();
     });
 
+    // Initialize translation variables and matrices
     var translationX = 0.0;
     var translationY = 0.0;
     var translationZ = 0.0;
@@ -241,22 +236,18 @@ var init = function () {
 
     // Function to update the translation when the slider value changes
     function updateTranslation() {
-        // Update the translation matrix
+        // Update the translation matrix and redraw the object
         mat4.identity(translationMatrix);
         mat4.translate(translationMatrix, translationMatrix, [translationX, translationY, translationZ]);
-
-        // Redraw the object
         gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
         mat4.mul(worldMatrix, yRotationMatrix, xRotationMatrix);
-
-        // Apply both the translation and previous transformations
         mat4.mul(worldMatrix, translationMatrix, worldMatrix);
         mat4.mul(worldMatrix, scaleMatrix, worldMatrix);
         gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
         gl.drawElements(gl.TRIANGLES, pyramidIndices.length, gl.UNSIGNED_SHORT, 0);
     }
 
-    // Get references to the translation slider and its value display span
+    // Get references to the translation sliders and their value display spans
     var translationXSlider = document.getElementById('translationXSlider');
     var translationXValue = document.getElementById('translationXValue');
     var translationYSlider = document.getElementById('translationYSlider');
@@ -264,29 +255,19 @@ var init = function () {
     var translationZSlider = document.getElementById('translationZSlider');
     var translationZValue = document.getElementById('translationZValue');
 
-    // Function to update the translationX variable and call updateTranslation
-    function onTranslationXChange() {
+    // Function to update the translation variable and call updateTranslation
+    function onTranslationChange() {
         translationX = parseFloat(translationXSlider.value);
         translationXValue.textContent = translationX.toFixed(1);
-        updateTranslation();
-    }
-
-    // Function to update the translationY variable and call updateTranslation
-    function onTranslationYChange() {
         translationY = parseFloat(translationYSlider.value);
         translationYValue.textContent = translationY.toFixed(1);
-        updateTranslation();
-    }
-
-    // Function to update the translationZ variable and call updateTranslation
-    function onTranslationZChange() {
         translationZ = parseFloat(translationZSlider.value);
         translationZValue.textContent = translationZ.toFixed(1);
         updateTranslation();
     }
 
-    // Update the translation when the slider value changes
-    translationXSlider.addEventListener('input', onTranslationXChange);
-    translationYSlider.addEventListener('input', onTranslationYChange);
-    translationZSlider.addEventListener('input', onTranslationZChange);
+    // Update the translation when the slider values change
+    translationXSlider.addEventListener('input', onTranslationChange);
+    translationYSlider.addEventListener('input', onTranslationChange);
+    translationZSlider.addEventListener('input', onTranslationChange);
 };
